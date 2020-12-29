3644a3c533b2662fe41891e7aa2be51d116e16f0
Rafael Treiber <rafaeltreiber@gmail.com>
2020-12-29 10:08:10 -0300

    layout pronto

---
+++ src/components/Menu/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
+
+export const Container = styled.ScrollView`    
+margin: 0 30px;  
+`;
+
+export const Code = styled.View`
+overflow: hidden;  
+background: #FFF;
+padding: 10px;
+align-self: center;
+`;
+
+export const Nav = styled.View`
+margin-top: 30px;
+align-self: stretch;
+border-top-width: ${StyleSheet.hairlineWidth}px;
+border-top-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavItem = styled.View`
+flex-direction: row;
+align-items: center;
+padding: 12px 8px;
+border-bottom-width: ${StyleSheet.hairlineWidth}px;
+border-bottom-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavText = styled.Text`
+font-size: 15px;
+color: #FFF;
+margin-left: 20px;
+`;
+
+export const SignOutButton = styled.TouchableOpacity`
+border-width: ${StyleSheet.hairlineWidth}px;
+border-color: rgba(255, 255, 255, 0.8);
+border-radius: 4px;
+justify-content: center;
+align-items: center;
+padding: 12px;
+margin-top: 15px;
+`;
+
+export const SignOutButtonText = styled.Text`
+font-size: 13px;
+color: #FFF;
+font-weight: bold;
+`;
